d923cace544fc8f6cb1a62afa61178c0eee40030
Ionică Bizău <bizauionica@gmail.com>
2016-04-10 17:23:15 +0300

    1.1.0
    Added the delimiter option.

--- example/index.js
+++ example/index.js
@@ -7,4 +7,13 @@ console.log(flatten({
     }
   , age: 20
 }));
-// { 'name.first': 'Johnny', 'name.last': 'B.', age: 20 }
+// => { 'name.first': 'Johnny', 'name.last': 'B.', age: 20 }
+
+console.log(flatten({
+    name: {
+        first: "Johnny"
+      , last: "B."
+    }
+  , age: 20
+}, "_"));
+// => { name_first: 'Johnny', name_last: 'B.', age: 20 }
